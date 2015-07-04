ec959ca82cdd8ff93d45147d0281d510683c5a98
Rush <rushidadhaniya@gmail.com>
2015-07-04 14:47:31 +0530

    Streams and pipes

--- Day-1/03-Print file/program.js
+++ Day-1/03-Print file/program.js
@@ -1,4 +1,5 @@
 var fs = require("fs");
+/*
 
 fs.readFile("test.txt", {encoding : "utf-8"}, function(err, contents) {
     
@@ -8,4 +9,16 @@ fs.readFile("test.txt", {encoding : "utf-8"}, function(err, contents) {
     
     console.log(contents);
     
-})
+})*/
+
+var readCount = 0;
+var stream = fs.createReadStream("test.txt", {encoding : "utf-8"});
+/*stream.on("data", function(chunk) {
+    ++readCount;
+   console.log(chunk); 
+});
+stream.on("end", function() {
+   console.log("EOF with readCount : " + readCount); 
+});*/
+
+stream.pipe(process.stdout);
